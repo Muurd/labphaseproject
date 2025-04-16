18ca63c6cd8f46c59f34ef0f0f5c850a7353da96
MURD KING <zieddebbabi12@gmail.com>
2025-04-16 14:18:36 +0100

    Serve frontend build and fix deployment port

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const app = express()
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const authRoutes = require('./routes/Auth.routes');
 require('dotenv').config()
 const productRoutes = require('./routes/Product.routes')
 const OrderRoutes = require('./routes/Order.routes')
+const path = require('path');
 
 
 app.use(express.json())
@@ -14,6 +15,11 @@ app.use(cors())
 app.use('/api/auth', authRoutes);
 app.use('/api/products',productRoutes)
 app.use('/api/orders',OrderRoutes)
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('connection to MongoDb is succesful'))
@@ -22,6 +28,7 @@ mongoose.connect(process.env.MONGO_URI)
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`)
 })
+
 process.on('SIGINT', () => {
     console.log("Shutting down the server");
     mongoose.connection.close(() => {
